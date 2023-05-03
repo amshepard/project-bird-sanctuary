544ca06f9b2b573b9d51fcd43ef926fa77846a14
amshepard <antoineshepard@pursuit.org>
2023-05-03 19:45:21 -0400

    Cart is semi-functional.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-
+// Importing the CSS stylesheet of the app
 import './App.css'
+
+// Importing the useState hook from React library
 import { useState } from "react";
-import birdData from './data/birds';
+
+// Importing the birdData object from the birds.js file
+import birdData from "./data/birds";
+
+// Importing the BirdCard and Cart components
 import BirdCard from "./Components/BirdCard";
 import Cart from "./Components/Cart";
 
+// Defining the App function component
+function App() {
 
-function App () {
+  // Declaring the state variables in order to manage the cart content and total
+  const [cartContent, setCartContent] = useState([]);
+  const [cartTotal, setCartTotal] = useState(null);
 
-    // Declaring the state variables in order to manage the cart content and total
-    const [cartContent, setCartContent] = useState([]);
-    const [cartTotal, setCartTotal] = useState(null);
-  
-    // Logging the current values of cartContent and cartTotal to the console
-    console.log(cartContent, cartTotal);
-  
-    // Defining the adoptBird function which adds a bird to the cart
-    function adoptBird(bird) {
-      setCartTotal(cartTotal + bird.amount);
-      setCartContent([...cartContent, bird]);
-    }
+  // Logging the current values of cartContent and cartTotal to the console
+  console.log(cartContent, cartTotal);
 
-      // Defining the deleteBird function which removes a bird from the cart
+  // Defining the adoptBird function which adds a bird to the cart
+  function adoptBird(bird) {
+    setCartTotal(cartTotal + bird.amount);
+    setCartContent([...cartContent, bird]);
+  }
+
+  // Defining the deleteBird function which removes a bird from the cart
   function deleteBird(birdAdded, i) {
     // filtering out the bird that needs to be deleted from the cartContent
     const filteredCart = cartContent.filter((item, index) => {
@@ -32,9 +38,9 @@ function App () {
     console.log(cartContent)
   }
 
-
+  // Rendering the JSX elements
   return (
-    <div>
+    <div className="app">
       <h1 className='header'>Hypothetical Hippogriff Haven</h1>
       <style> @import url('https://fonts.googleapis.com/css2?family=Bangers&display=swap');</style>
       <Cart
@@ -43,20 +49,21 @@ function App () {
         cartTotal={cartTotal}
         deleteBird={deleteBird}
       />
-      <div className='birdcard'>
-      {
-        birdData.sort((a, b) => b.amount - a.amount).map((bird) => {
-          return <BirdCard
-            key={bird.amount}
-            bird={bird}
-            adoptBird={adoptBird}
-          />
-        })
-      }
 
+      <div className='birdcard'>
+        {
+          birdData.map((bird) => {
+            return <BirdCard
+              key={bird.id}
+              bird={bird}
+              adoptBird={adoptBird}
+            />
+          })
+        }
       </div>
     </div>
   );
 };
 
+// Exporting the App component
 export default App;
